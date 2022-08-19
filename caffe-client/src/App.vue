5f1f745d75a80bdbfd2e81362bea144d60abe7c4
salwazahramunir <salwazahramunir@gmail.com>
2022-08-19 19:02:06 +0700

    first commit

---
+++ caffe-client/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import { RouterView } from 'vue-router'
+
+export default {
+  created() {
+    if (localStorage.access_token) {
+      this.$router.push({ name: 'dashboard' })
+    } else {
+      this.$router.push({ name: 'login' })
+    }
+  }
+}
+</script>
+<template>
+  <RouterView />
+</template>
---
+++ caffe-client/src/main.js
@@ -0,0 +1,18 @@
+import { createApp, markRaw } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+pinia.use(({ store }) => {
+  store.router = markRaw(router)
+})
+app.use(router)
+
+app.mount('#app')
