99f7246021f6fa50786823c4a77dc9d12580769f
salwazahramunir <salwazahramunir@gmail.com>
2022-08-19 20:21:39 +0700

    logout feature done

--- caffe-client/src/components/NavBar.vue
+++ caffe-client/src/components/NavBar.vue
@@ -1,6 +1,14 @@
 <script>
-export default {
+import { mapActions } from 'pinia'
+import { useAuthStore } from '@/stores/authStore.js'
 
+export default {
+    methods: {
+        ...mapActions(useAuthStore, ["logout"]),
+        handleLogout() {
+            this.logout()
+        }
+    }
 }
 </script>
 <template>
@@ -24,7 +32,7 @@ export default {
                             <i class="ti-settings text-primary"></i>
                             Settings
                         </a>
-                        <a class="dropdown-item">
+                        <a class="dropdown-item" @click.prevent="handleLogout">
                             <i class="ti-power-off text-primary"></i>
                             Logout
                         </a>
